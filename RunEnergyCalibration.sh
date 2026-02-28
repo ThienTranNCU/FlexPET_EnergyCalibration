4391d4374e20c23c197585f58263bf11f7c39ea5
ThienTranNCU <thientran1234tct@gmail.com>
2026-02-28 21:28:44 +0800

    Adding data to gitignore

--- RunEnergyCalibration.sh
+++ RunEnergyCalibration.sh
@@ -2,7 +2,7 @@
 
 g++ FlexPET_EnergyCal_v2.cpp -o FlexPET_EnergyCal_v2 `root-config --cflags --glibs` -lSpectrum
 
-./FlexPET_EnergyCal_v2 <InputFolder>
+./FlexPET_EnergyCal_v2 data
 
 #306.82
 #511
